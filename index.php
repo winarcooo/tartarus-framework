b7b74094383258a8224eee5fe94c6a9d48c1eb68
winarkoandy <winarcooo@gmail.com>
2016-02-11 10:54:36 +0700

    initial project

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+// tartarus/index.php
+require_once __DIR__.'/vendor/autoload.php';
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+$request = Request::createFromGlobals();
+
+$input = $request->get('name','world');
+
+$response = new Response(sprintf('Hello %s', htmlspecialchars($input, ENT_QUOTES, 'UTF-8')));
+//$response = new Response('Goodbye!');
+
+$response->send();
