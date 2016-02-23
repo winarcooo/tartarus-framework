ed70f9173cdc45682a891d9257c739166c684d94
winarkoandy <winarcooo@gmail.com>
2016-02-24 01:51:51 +0700

    implement symfony routing

---
+++ src/app.php
@@ -0,0 +1,9 @@
+<?php
+//tartarus/src/app.php
+use Symfony\Component\Routing;
+
+$routes = new Routing\RouteCollection();
+$routes->add('hello', new Routing\Route('/hello/{name}',array('name'=>'world')));
+$routes->add('bye', new Routing\Route('/bye'));
+
+return $routes;
