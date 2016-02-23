7316220e261e37749c6617ff8e36650517f65f3b
winarkoandy <winarcooo@gmail.com>
2016-02-24 01:12:57 +0700

    add symfony routing

--- src/pages/hello.php
+++ src/pages/hello.php
@@ -1,4 +1,3 @@
 <!-- example.com/src/pages/hello.php -->
-<?php $name = $request->get('name','world') ?>
 
 Hello <?php echo htmlspecialchars($name, ENT_QUOTES, 'UTF-8') ?>
--- web/front.php
+++ web/front.php
@@ -10,18 +10,19 @@ $request = Request::CreateFromGlobals();
 $response = new Response();
 
 $map = [
-    '/hello' => __DIR__.'/../src/pages/hello.php',
-    '/bye'  => __DIR__.'/../src/pages/bye.php',
+    '/hello' => 'hello',
+    '/bye'  => 'bye',
 ];
 
 $path = $request->getPathInfo();
 
 if(isset($map[$path])) {
-    include $map[$path];
-    $response->setContent(ob_get_clean());
+    ob_start();
+    extract($request->query->all(), EXTR_SKIP);
+    include sprintf(__DIR__.'/../src/pages/%s.php', $map[$path]);
+    $response = new Response(ob_get_clean());
 } else {
-    $response->setStatusCode(404);
-    $response->setContent('page not found');
+    $response = new Response('Not Found', 404);
 }
 
 $response->send();
