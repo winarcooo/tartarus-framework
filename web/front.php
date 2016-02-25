1d67b440298af01432eebadf4f33949b605099c5
winarkoandy <winarcooo@gmail.com>
2016-02-25 21:27:27 +0700

    test create some function

--- web/front.php
+++ web/front.php
@@ -6,6 +6,15 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing;
 
+function renderTemplate($request)
+{
+    extract($request->attributes->all(), EXTR_SKIP);
+    ob_start();
+    include sprintf(__DIR__.'/../src/pages/%s.php', $_route);
+    
+    return new Response(ob_get_clean());
+}
+
 $request = Request::CreateFromGlobals();
 $routes = include __DIR__.'/../src/app.php';
 
@@ -14,14 +23,11 @@ $context->fromRequest($request);
 $matcher = new Routing\Matcher\UrlMatcher($routes, $context);
 
 try{
-    extract($matcher->match($request->getPathInfo()), EXTR_SKIP);
-    ob_start();
-    include sprintf(__DIR__.'/../src/pages/%s.php',$_route);
-    
-    $response = new Response(ob_get_clean());
-}catch(Routing\Exception\ResourceNotFoundException $e) {
+    $request->attributes->add($matcher->match($request->getPathInfo()));
+    $response = call_user_func($request->attributes->get('_controller'), $request);
+} catch (Routing\Exception\ResourceNotFoundException $e) {
     $response = new Response('Not Found',404);
-}catch(Exception $e) {
+} catch (Exception $e) {
     $response = new Response('An error occurred',500);
 }
 
